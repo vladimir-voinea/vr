b8bc33ff05627b21fb63307ae4754e2c34d18e3b
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-08 11:31:33 +0300

    add glm 0.9.9.8 source

--- vr/vr.cpp
+++ vr/vr.cpp
@@ -2,14 +2,24 @@
 //
 
 #include "vr.h"
+
 #include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 int main()
 {
     glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Working window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
