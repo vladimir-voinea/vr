e94733862b37909a7a6c3f90554674a350671b24
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-08 12:11:37 +0300

    link vulkan with cmake

--- vr/vr.cpp
+++ vr/vr.cpp
@@ -3,6 +3,7 @@
 
 #include "vr.h"
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
@@ -16,6 +17,11 @@ int main()
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
 
 	glm::mat4 matrix;
 	glm::vec4 vec;
