f40d368eb9b56cb82f2f6288ff08f7b6373c2b2f
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-08 11:15:37 +0300

    add glfw3 source

---
+++ vr/vr.cpp
@@ -0,0 +1,13 @@
+﻿// vr.cpp : Defines the entry point for the application.
+//
+
+#include "vr.h"
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
