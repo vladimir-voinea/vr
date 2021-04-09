1d54ff19c4ab1f5be9d3242d496eb2d7d9714242
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-09 12:27:21 +0300

    add support to check for layer extensions

--- vr/include/vk_extensions.hpp
+++ vr/include/vk_extensions.hpp
@@ -3,8 +3,11 @@
 #include <vulkan/vulkan.h>
 
 #include <vector>
+#include <string>
 
 namespace vr
 {
 	std::vector<VkExtensionProperties> get_supported_extensions();
+
+	std::vector<VkExtensionProperties> get_supported_extensions_for_layer(const std::string& layer_name);
 }
--- vr/src/vk_extensions.cpp
+++ vr/src/vk_extensions.cpp
@@ -1,16 +1,17 @@
 #include "vk_extensions.hpp"
 
+#include <optional>
 #include <stdexcept>
 
-namespace vr
+namespace
 {
-	std::vector<VkExtensionProperties> get_supported_extensions()
+	std::vector<VkExtensionProperties> get_extensions(const std::optional<std::string> layer_name)
 	{
 		uint32_t extension_count = 0;
-		if (const auto got_count = vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr); got_count == VK_SUCCESS)
+		if (const auto got_count = vkEnumerateInstanceExtensionProperties(layer_name ? layer_name->c_str() : nullptr, &extension_count, nullptr); got_count == VK_SUCCESS)
 		{
 			std::vector<VkExtensionProperties> extensions(extension_count);
-			if (const auto got_extensions = vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data()); got_extensions == VK_SUCCESS)
+			if (const auto got_extensions = vkEnumerateInstanceExtensionProperties(layer_name ? layer_name->c_str() : nullptr, &extension_count, extensions.data()); got_extensions == VK_SUCCESS)
 			{
 				return extensions;
 			}
@@ -23,5 +24,18 @@ namespace vr
 		{
 			throw std::runtime_error("Could not get supported extensions count");
 		}
+	}
+}
+
+namespace vr
+{
+	std::vector<VkExtensionProperties> get_supported_extensions()
+	{
+		return get_extensions({});
+	}
+
+	std::vector <VkExtensionProperties> get_supported_extensions_for_layer(const std::string& layer_name)
+	{
+		return get_extensions(layer_name);
 	}
 }
