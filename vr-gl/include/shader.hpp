d65688107337d128ef35d2f76dbb1567ec697f03
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-10 12:19:12 +0300

    rename vr-ogl to vr-gl

---
+++ vr-gl/include/shader.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+//#include <gl/gl.h>
+
+#include <string>
+
+namespace vr::gl
+{
+	class shader
+	{
+	public:
+		shader(const std::string& source);
+		~shader();
+
+	private:
+	};
+}
---
+++ vr-gl/src/shader.cpp
@@ -0,0 +1,16 @@
+#include "shader.hpp"
+
+//#include <gl/GL.h>
+
+namespace vr::gl
+{
+	shader::shader(const std::string& source)
+	{
+		//m_shader = glCreateShader()
+	}
+
+	shader::~shader()
+	{
+
+	}
+}
