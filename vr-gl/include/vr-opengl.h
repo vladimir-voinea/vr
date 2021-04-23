e9f425374cdffdc6ac5f78a8c64a85f6ae58b15d
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-23 21:01:43 +0300

    fix android ifdef

--- vr-gl/include/vr-opengl.h
+++ vr-gl/include/vr-opengl.h
@@ -3,7 +3,7 @@
 #ifdef _WIN32
 	#define GLEW_STATIC
 	#include <gl/glew.h>
-#else ifdef __ANDROID__
+#elif __ANDROID__
 	#if __ANDROID_API__ >= 24
 	#include <GLES3/gl32.h>
 	#elif __ANDROID_API__ >= 21
