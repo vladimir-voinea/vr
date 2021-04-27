69fb86412e61feab4901cc371edbf5a0d80c768a
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-27 15:18:40 +0300

    can now open assets from android

--- vr-platform/src/android_asset_manager.cpp
+++ vr-platform/src/android_asset_manager.cpp
@@ -6,16 +6,22 @@
 
 #include <spdlog/spdlog.h>
 
+#include <stdexcept>
+
 namespace vr::platform
 {
     android_asset_manager::android_asset_manager()
         : m_asset_manager(AAssetManager_fromJava(get_jni_env(), get_asset_manager()))
     {
+        if (!m_asset_manager)
+        {
+            throw std::runtime_error("Null asset manager");
+        }
     }
 
 	asset android_asset_manager::get_asset_by_name(const std::string& name)
 	{
-		return "file:///android_asset/" + name;
+		return name;
 	}
 
 	std::vector<uint8_t> android_asset_manager::read_file(const asset& asset)
@@ -23,7 +29,7 @@ namespace vr::platform
         std::vector<uint8_t> result;
 
         if (AAsset* a = AAssetManager_open(m_asset_manager, asset.get_path().c_str(), AASSET_MODE_UNKNOWN); a != nullptr) {
-            spdlog::info("Successfully opened asset {0}", asset.get_path());
+            spdlog::debug("Successfully opened asset {0}", asset.get_path());
             const auto size = AAsset_getLength(a);
             result.resize(size);
             AAsset_read(a, result.data(), result.size());
