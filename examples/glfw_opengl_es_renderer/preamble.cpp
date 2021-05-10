ffbef31b9f1aa3561ab6f5ac9b2ac46e1c9337c1
Vladimir Voinea <voineavladimir@gmail.com>
2021-05-10 17:34:16 +0300

    imgui is working on windows

--- examples/glfw_opengl_es_renderer/preamble.cpp
+++ examples/glfw_opengl_es_renderer/preamble.cpp
@@ -1,6 +1,12 @@
 #include "preamble.hpp"
 #include <glfw_util.hpp>
 
+#include <spdlog/spdlog.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <stdexcept>
 
 preamble::preamble(vr::glfw::window& window)
@@ -25,6 +31,11 @@ void preamble::initialize()
 	m_main_loop = std::make_unique<main_loop>(viewport{ 0, 0, viewport_size.width, viewport_size.height });
 	m_input_listener = std::make_unique<input_listener>(m_window, m_main_loop->get_camera(), m_fps_counter);
 	initialize_controls();
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(m_window.get_handle(), false);
+	ImGui_ImplOpenGL3_Init("#version 300 es");
 }
 
 void preamble::initialize_glew()
@@ -64,6 +75,23 @@ void preamble::run_loop()
 
 			process_input();
 			m_main_loop->frame(delta_time);
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			
+			float f;
+			ImGui::Text("Hello, world %d", 123);
+			if (ImGui::Button("Save")) {
+				spdlog::info("Save clicked!");
+			}
+			char buf[256] = "asdf";
+			ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 			m_window.swap_buffers();
 		}
 
