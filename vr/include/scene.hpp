9a6e1d9f42956a17d2ad432effc7810ae3420a8c
Vladimir Voinea <voineavladimir@gmail.com>
2021-05-14 23:22:17 +0300

    add lights to scene as non-owning pointers

--- vr/include/scene.hpp
+++ vr/include/scene.hpp
@@ -2,6 +2,13 @@
 
 #include "object3d.hpp"
 
+#include "ambient_light.hpp"
+#include "directional_light.hpp"
+#include "point_light.hpp"
+#include "spot_light.hpp"
+
+#include <vector>
+
 namespace vr
 {
 	class scene : public object3d
@@ -10,6 +17,20 @@ namespace vr
 		scene();
 		~scene() override;
 
+		void add(const ambient_light* light);
+		void add(const directional_light* light);
+		void add(const point_light* light);
+		void add(const spot_light* light);
+
+		void remove(const ambient_light* light);
+		void remove(const directional_light* light);
+		void remove(const point_light* light);
+		void remove(const spot_light* light);
+
 	private:
+		const ambient_light* m_ambient_light = nullptr;
+		const directional_light* m_directional_light = nullptr;
+		std::vector<const point_light*> m_point_lights;
+		std::vector<const spot_light*> m_spot_lights;
 	};
 }
--- vr/src/scene.cpp
+++ vr/src/scene.cpp
@@ -7,4 +7,46 @@ namespace vr
 	{}
 
 	scene::~scene() = default;
+
+	void scene::add(const ambient_light* light)
+	{
+		m_ambient_light = light;
+	}
+
+	void scene::add(const directional_light* light)
+	{
+		m_directional_light = light;
+	}
+
+	void scene::add(const point_light* light)
+	{
+		m_point_lights.push_back(light);
+	}
+
+	void scene::add(const spot_light* light)
+	{
+		m_spot_lights.push_back(light);
+	}
+
+
+	void scene::remove(const ambient_light* light)
+	{
+		m_ambient_light = nullptr;
+	}
+
+	void scene::remove(const directional_light* light)
+	{
+		m_directional_light = nullptr;
+	}
+
+	void scene::remove(const point_light* light)
+	{
+		m_point_lights.erase(std::remove(m_point_lights.begin(), m_point_lights.end(), light));
+	}
+
+	void scene::remove(const spot_light* light)
+	{
+		m_spot_lights.erase(std::remove(m_spot_lights.begin(), m_spot_lights.end(), light));
+	}
+
 }
